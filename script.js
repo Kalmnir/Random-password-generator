eac8805d425d1273c43a81696fa27d859f989731
John-Mark Sackett <jmsackett91@gmail.com>
2021-06-16 21:53:51 -0400

    added first prompt

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  prompt("Please input desired password length");
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
