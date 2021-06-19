f50e716fa2bf43ad21aca4a74f7e390cb5a88c6d
John-Mark Sackett <jmsackett91@gmail.com>
2021-06-18 21:16:27 -0400

    re-structured java slightly and added prompts for password options

--- script.js
+++ script.js
@@ -1,25 +1,47 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,];
 var special = ["\u0021", "\u0022", "\u0023", "\u0024", "\u0025", "\u0026", "\u0027", "\u0028", "\u0029", "\u002A", "\u002B", "\u002C", "\u002D", "\u002E", "\u002F", "\u003A", "\u003B", "\u003C", "\u003D", "\u003E", "\u003F", "\u0040", "\u005B", "\u005C", "\u005D", "\u005E", "\u005F", "\u0060", "\u007B", "\u007C", "\u007D", "\u007E"];
 var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var yourPassword = [];
 
-
-// Write password to the #password input
-function writePassword() {
-  userChoice = window.prompt("Please input desired password length in numerals");
+function generatePassword() {
+  userChoice = (prompt("How long do you want your password?"));
   if (!userChoice) {
     return;
   }
-  if (userChoice < 8) {
-    window.alert("password must be at least 8 characters long")
+  if (userChoice < 8 || userChoice > 128) {
+    alert("password must be between 8 and 128 characters long")
     return;
-  } else if (userChoice > 128) {
-    window.alert("Password cannot be more than 128 characters long")
-    return;
-  } else
-    window.confirm("Do you want your password to contain numbers?  Okay for yes, Cancel for no");
+  }
+
+
+  var passNumbers = confirm("Do you want your password to contain numbers?  Okay for yes, Cancel for no");
+  if (passNumbers === true);
+
+  var passSpecial = confirm("Do you want your password to contain special characters?  Okay for yes, Cancel for no");
+  if (passSpecial === true);
+
+
+  var passLower = confirm("Do you want your password to contain lowercase letters?  Okay for yes, Canel for no");
+  if (passLower === true);
+
+
+  var passUpper = confirm("Do you want your password to contain uppercase letters?  Okay for yes, Cancel for no");
+  if (passUpper === true);
+
+
+  if (!passNumbers && !passSpecial && !passLower && !passUpper) {
+    return "Password must contain at least one of the assigned variables.";
+  }
+}
+
+
+// Write password to the #password input
+var generateBtn = document.querySelector("#generate");
+
+function writePassword() {
 
 
 
@@ -31,4 +53,6 @@ function writePassword() {
 }
 
 // Add event listener to generate button
+
+
 generateBtn.addEventListener("click", writePassword);
